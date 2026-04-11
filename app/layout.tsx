5cae09d02ed92261b782c693fbeb0b45aaba1a96
Ahmet Selman Şener <selmansenerr@gmail.com>
2026-04-11 13:37:51 +0300

    feat: initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
-import { Libre_Baskerville, Geist } from "next/font/google";
+import { Libre_Baskerville, Geist, Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 
-const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,8 +16,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={cn("font-sans", geist.variable)}>
-      <body className={`${geist.variable} antialiased`}>
+    <html lang="en" className={cn("font-sans", inter.variable)}>
+      <body className={`${inter.variable} antialiased`}>
         {children}
       </body>
     </html>
